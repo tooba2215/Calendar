fc31e5630ae86126828fdb9cbaa7a1dd8d3f9651
Tooba <tooba.iqbal@soutvillesol.com>
2023-04-27 19:17:16 +0500

    First Commit

---
+++ db/migrate/20230110092319_create_calendars.rb
@@ -0,0 +1,11 @@
+class CreateCalendars < ActiveRecord::Migration[7.0]
+  def change
+    create_table :calendars do |t|
+      t.string :name
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
